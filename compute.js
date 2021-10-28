11c6a5bb1b46ff8f009560e6b95d7b22371c2ce3
Sai Sharma <msaisharma9@gmail.com>
2021-10-27 23:40:21 -0500

    added bonus

--- compute.js
+++ compute.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var storeValue;
+var storeValue1;
+var storeValue2;
 /* GET home page. */
 router.get('/', function (req, res, next) {
     fake_url = "https://fake.com/path" + req.url
@@ -12,8 +13,8 @@ router.get('/', function (req, res, next) {
     if (storeValue == 0)
         storeValue = Math.random()
     res.write("Computes the values for Math.atan2 and math.cbrt function." + "\n")
-    res.write('Math.atan2 applied to ' + storeValue + " is " + Math.atan2(storeValue) + "\n");
-    res.end('Math.cbrt applied to ' + storeValue + " is " + Math.cbrt(storeValue));
+    res.write('Math.atan2 applied to ' + storeValue1 + " is " + Math.atan2(storeValue1) + "\n");
+    res.end('Math.cbrt applied to ' + storeValue2 + " is " + Math.cbrt(storeValue2));
 });
 
 module.exports = router;
