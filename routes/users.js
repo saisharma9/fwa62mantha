6d694f43fc208c6fbb9d3e9d30efa469370b3026
Sai Sharma <msaisharma9@gmail.com>
2021-10-27 23:28:44 -0500

    new endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var variable = 0;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  variable = variable + 1;
+  res.send(`users accesses are: ${variable}`);
 });
 
 module.exports = router;
